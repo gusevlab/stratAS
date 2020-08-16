2b3f9e4dcc3396d9125c06effda9aaae6ad66860
Sasha Gusev <alexander_gusev@dfci.harvard.edu>
2020-08-16 13:19:14 -0400

    Update creating_ase_vcf.sh

--- pipeline/creating_ase_vcf.sh
+++ pipeline/creating_ase_vcf.sh
@@ -25,7 +25,7 @@ set +o posix
 zcat $VCF | awk -vs=$SAMPLE 'BEGIN{ OFS="\t" } { if(substr($1,1,1) != "#") exit 1; if($1 == "#CHROM") { print "##FORMAT=<ID=AS,Number=2,Type=Integer,Description=\"Read counts for the ref and alt alleles\">"; print $1,$2,$3,$4,$5,$6,$7,$8,$9,s; } else print $0; }' > $OUT.vcf
 
 # Join the body
-join -1 1 -2 1 \
+join -a1 -1 1 -2 1 \
 <(zcat $VCF | awk 'substr($1,1,1) != "#" { printf $1":"$2" "; print $0 }' | sed 's/chr//' | sort -k1,1) \
 <(cat $CSV | tail -n+2 | awk '{ printf $1":"$2" "; printf "%d %d\n",$6,$7 }' | sed 's/chr//' | sort -k1,1) \
 | cut -d ' ' -f2- \
